b84e7df6f26422de144e47e7f06d86e5fc03cbe1
Chitrarth <chitrarthrai10@gmail.com>
2024-06-11 13:33:00 +0530

    bentogrid compeleted with lottie and magicbutton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import { Spotlight } from "./ui/Spotlight";
 import React from "react";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import MagicButton from "./ui/MagicButton";
+import MagicButton from "./MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
