35089987036682730ffe20849d8b65944989230e
Chitrarth <chitrarthrai10@gmail.com>
2024-06-19 00:01:46 +0530

    customized RecentProject.tsx and add sentry for user to report bug

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import dynamic from "next/dynamic";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 export default function Home() {
   const Grid = dynamic(() => import("@/components/Grid"), {
     ssr: false,
@@ -11,9 +12,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
-          <FloatingNav navItems={[
-            {name: 'Home', link: '/', icon: <FaHome/>}
-            ]}/>
+          <FloatingNav navItems={navItems}/>
           <Hero/>
           <Grid/>
           <RecentProjects/>
